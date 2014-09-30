fc3f2d2013caedad2189fd366abaa2b1b2f262c7
John Donovan <jjdonov@gmail.com>
2014-09-29 22:42:40 -0700

    Adding tests.

---
+++ test/com/gtnexus/appxpress/pmbuilder/ArgsAndPropertiesConsolidatorTest.java
@@ -0,0 +1,18 @@
+package com.gtnexus.appxpress.pmbuilder;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+/**
+ * Created by jjdonov on 9/29/14.
+ */
+public class ArgsAndPropertiesConsolidatorTest {
+
+    @Test
+    public void testWithArgsAndNullProps() {
+        Properties properties = new Properties();
+        String[] args = {""};
+    }
+
+}
