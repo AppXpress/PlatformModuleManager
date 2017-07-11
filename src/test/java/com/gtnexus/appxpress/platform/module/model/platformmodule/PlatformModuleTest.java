db54e869b5e480bab17795705cb36e5fb2af3c0a
John Donovan <john.donovan@gtnexus.com>
2017-07-11 16:42:17 -0700

    Fixes broken unit test

--- src/test/java/com/gtnexus/appxpress/platform/module/model/platformmodule/PlatformModuleTest.java
+++ src/test/java/com/gtnexus/appxpress/platform/module/model/platformmodule/PlatformModuleTest.java
@@ -8,20 +8,21 @@ import java.io.FileReader;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.JAXBIntrospector;
 import javax.xml.bind.Unmarshaller;
 
 import org.junit.Test;
 
 public class PlatformModuleTest {
-	
-	@Test
-	public void testUnmarshall() throws FileNotFoundException, JAXBException {
-		File file = new File("src/test/resources/PlatformModule.xml");
-		FileReader reader = new FileReader(file);
-        JAXBContext context = JAXBContext.newInstance(PlatformModuleXml.class);
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        PlatformModuleXml mod = (PlatformModuleXml)unmarshaller.unmarshal(reader);
-        assertNotNull(mod);
-	}
+
+    @Test
+    public void testUnmarshall() throws FileNotFoundException, JAXBException {
+	File file = new File("src/test/resources/PlatformModule.xml");
+	FileReader reader = new FileReader(file);
+	JAXBContext context = JAXBContext.newInstance("com.gtnexus.appxpress.platform.module.model.platformmodule");
+	Unmarshaller unmarshaller = context.createUnmarshaller();
+	PlatformModuleXml platformMod = (PlatformModuleXml) JAXBIntrospector.getValue(unmarshaller.unmarshal(reader));
+	assertNotNull(platformMod);
+    }
 
 }
