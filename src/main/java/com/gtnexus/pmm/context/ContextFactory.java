9ec899f77c1f9d1598b303c5ab4591e77a81c34f
John Donovan <john.donovan@gtnexus.com>
2018-04-05 15:47:29 -0700

    comment out some dead code

--- src/main/java/com/gtnexus/pmm/context/ContextFactory.java
+++ src/main/java/com/gtnexus/pmm/context/ContextFactory.java
@@ -1,39 +1,25 @@
 package com.gtnexus.pmm.context;
 
-import java.util.Map;
-import java.util.Set;
-
-import com.gtnexus.pmm.AppXpressDirResolver;
-import com.gtnexus.pmm.AppXpressException;
-import com.gtnexus.pmm.cli.option.CLICommandOption;
-import com.gtnexus.pmm.cli.option.CLICommandOptionInterpreter;
-import com.gtnexus.pmm.cli.option.CLICommandOptionParser;
-import com.gtnexus.pmm.cli.option.ParsedOptions;
-import com.gtnexus.pmm.commons.DirectoryHelper;
-import com.gtnexus.pmm.commons.command.PMMCommandInfo;
-import com.gtnexus.pmm.commons.runtime.SimpleShutdown;
-import com.gtnexus.pmm.commons.runtime.SimpleShutdownImpl;
-
 public class ContextFactory {
     
     private ContextFactory() {}
 
-    private static final AppXpressDirResolver resolver = new AppXpressDirResolver();
-    private static final InterpreterFactory interpreterFac = new InterpreterFactory(resolver);
+//    private static final AppXpressDirResolver resolver = new AppXpressDirResolver();
+//    private static final InterpreterFactory interpreterFac = new InterpreterFactory(resolver);
 
-    public static PmmContext createContext(PMMCommandInfo app, String[] args)
-	    throws AppXpressException {
-	//TODO: remove. this will be in start up...
-	DirectoryHelper dHelper = new DirectoryHelper();
-	dHelper.ensureAppXpress();
-	Set<CLICommandOption> options = app.getOptions();
-	CLICommandOptionParser parser = CLICommandOptionParser.createParser(options, args);
-	ParsedOptions parsedOptions = parser.parse();
-	SimpleShutdown shutdown = new SimpleShutdownImpl();
-	CLICommandOptionInterpreter interpreter = interpreterFac.createInterpreter(app, parsedOptions,
-		dHelper.getPmProperties());
-	Map<CLICommandOption, String> interpretedOptions = interpreter.interpret();
-	return new PmmContext(app, shutdown, dHelper.getPmProperties(), interpretedOptions);
-    }
+//    public static PmmContext createContext(PMMCommandInfo app, String[] args)
+//	    throws AppXpressException {
+//	//TODO: remove. this will be in start up...
+//	DirectoryHelper dHelper = new DirectoryHelper();
+//	dHelper.ensureAppXpress();
+//	Set<CLICommandOption> options = app.getOptions();
+//	CLICommandOptionParser parser = CLICommandOptionParser.createParser(options, args);
+//	ParsedOptions parsedOptions = parser.parse();
+//	SimpleShutdown shutdown = new SimpleShutdownImpl();
+//	CLICommandOptionInterpreter interpreter = interpreterFac.createInterpreter(app, parsedOptions,
+//		dHelper.getPmProperties());
+//	Map<CLICommandOption, String> interpretedOptions = interpreter.interpret();
+//	return new PmmContext(app, shutdown, dHelper.getPmProperties(), interpretedOptions);
+//    }
 
 }
--- src/main/java/com/gtnexus/pmm/context/InterpreterFactory.java
+++ src/main/java/com/gtnexus/pmm/context/InterpreterFactory.java
@@ -1,22 +1,5 @@
 package com.gtnexus.pmm.context;
 
-import java.io.File;
-
-import com.gtnexus.pmm.AppXpressDirResolver;
-import com.gtnexus.pmm.cli.FileSelector;
-import com.gtnexus.pmm.cli.Select;
-import com.gtnexus.pmm.cli.option.CLICommandOptionInterpreter;
-import com.gtnexus.pmm.cli.option.ParsedOptions;
-import com.gtnexus.pmm.commons.command.PMMCommandInfo;
-import com.gtnexus.pmm.commons.properties.PMProperties;
-import com.gtnexus.pmm.pmbuilder.cli.BuilderOption;
-import com.gtnexus.pmm.pmbuilder.cli.BuilderOptionInterpreter;
-import com.gtnexus.pmm.pmbuilder.exception.PMBuilderException;
-import com.gtnexus.pmm.pmdocgen.cli.DocumentGeneratorOptionInterpreter;
-import com.gtnexus.pmm.pmdocgen.cli.option.DocumentGeneratorOptions;
-import com.gtnexus.pmm.pmextractor.cli.ExtractorOption;
-import com.gtnexus.pmm.pmextractor.cli.ExtractorOptionInterpreter;
-
 /**
  * 
  * @author jdonovan
@@ -24,11 +7,11 @@ import com.gtnexus.pmm.pmextractor.cli.ExtractorOptionInterpreter;
  */
 public class InterpreterFactory {
 
-    private final AppXpressDirResolver resolver;
-
-    public InterpreterFactory(AppXpressDirResolver resolver) {
-	this.resolver = resolver;
-    }
+//    private final AppXpressDirResolver resolver;
+//
+//    public InterpreterFactory(AppXpressDirResolver resolver) {
+//	this.resolver = resolver;
+//    }
 
     /**
      * 
@@ -39,19 +22,19 @@ public class InterpreterFactory {
      * @return
      * @throws PMBuilderException
      */
-    public CLICommandOptionInterpreter createInterpreter(PMMCommandInfo app, ParsedOptions options, PMProperties properties)
-	    throws PMBuilderException {
-	Class<?> contextType = app.getContextType();
-	Select<File> selector = new FileSelector(System.in, System.out);
-	if (contextType.equals(BuilderOption.class)) {
-	    return new BuilderOptionInterpreter(app, options, properties, selector, resolver);
-	} else if (contextType.equals(ExtractorOption.class)) {
-	    return new ExtractorOptionInterpreter(app, options, properties, selector, resolver);
-	} else if (contextType.equals(DocumentGeneratorOptions.class)) {
-	    return new DocumentGeneratorOptionInterpreter(app,
-		     options, properties, selector, resolver);
-	}
-	throw new IllegalArgumentException("Unsupported context type.");
-    }
+//    public CLICommandOptionInterpreter createInterpreter(PMMCommandInfo app, ParsedOptions options, PMProperties properties)
+//	    throws PMBuilderException {
+//	Class<?> contextType = app.getContextType();
+//	Select<File> selector = new FileSelector(System.in, System.out);
+//	if (contextType.equals(BuilderOption.class)) {
+//	    return new BuilderOptionInterpreter(app, options, properties, selector, resolver);
+//	} else if (contextType.equals(ExtractorOption.class)) {
+//	    return new ExtractorOptionInterpreter(app, options, properties, selector, resolver);
+//	} else if (contextType.equals(DocumentGeneratorOptions.class)) {
+//	    return new DocumentGeneratorOptionInterpreter(app,
+//		     options, properties, selector, resolver);
+//	}
+//	throw new IllegalArgumentException("Unsupported context type.");
+//    }
 
 }
